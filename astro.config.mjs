bf2ab660c1f2d33308ad0b530e059ee39a7f716a
Ivelin Mitev <ivelins.m@abv.bg>
2026-05-12 01:25:09 +0300

    FIX - Navigation not working on github pages as url is missing /

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   site: isProd ? 'https://ivelinsm.github.io' : undefined,
-  base: isProd ? '/sji-web' : '/',
+  base: isProd ? '/sji-web/' : '/',
 });
