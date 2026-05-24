166178b2cc7a1068183a9665cbc3644bbf17e17e
Ivelin Mitev <ivelins.m@abv.bg>
2026-05-24 17:06:27 +0300

    CHORE - Prepare work for cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
-  site: isProd ? 'https://ivelinsm.github.io' : undefined,
-  base: isProd ? '/sji-web/' : '/',
+  site: 'https://sjigrup.com',
+  base: '/',
 });
